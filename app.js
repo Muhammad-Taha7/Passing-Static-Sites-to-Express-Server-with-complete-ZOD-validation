82c55ab15e8ee244379a7dc7cca8d833f5204e4e
Taha <taha@gmail.com>
2026-06-18 13:37:34 +0500

    new

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
 import { z } from 'zod';
 
-
 const envSchema = z.object({
-    Port: z.coerce.number().int().positive().default(3000),
+  PORT: z.coerce.number().int().positive().default(3000),
 });
 
 const env = envSchema.safeParse(process.env);
 
 if (!env.success) {
-    console.error('Invalid environment variables:', env.error.flatten());
-    process.exit(1);
+  console.error('Invalid environment variables:', env.error.flatten());
+  process.exit(1);
 }
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+// ✅ Serve static files
+app.use(express.static('public',"index.html","Style.css"));
+
+// optional route (not required)
+app.get('/api', (req, res) => {
+  res.json({ message: "API working fine 🚀" });
 });
 
-
-app.listen(env.data.Port, () => {
-    console.log(`Server is running on port ${env.data.Port}`);
+app.listen(env.data.PORT, () => {
+  console.log(`Server is running on port ${env.data.PORT}`);
 });
